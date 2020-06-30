49efed5ebbfb786a48265ff6526f21259cb1def3
Ronald <ronystivens@hotmail.com>
2020-06-30 23:58:59 +0200

    funciones con pytohon

---
+++ funciones.py
@@ -0,0 +1,24 @@
+
+
+def mensajero():
+    message="Hola Mundo, arrancando con phyton"
+    print(message)  
+
+mensajero()
+
+
+def suma():
+    num1=2
+    num2=3
+    print(num1+num2)
+
+suma()
+
+
+
+def suma2(num1,num2):
+    print(num1+num2)
+
+suma2(5,7)
+
+
